4b867d6c9caacf5690f4a6f55a12b19c760e5a1c
SSTC-bit <waltdisneyentertainmentco@gmail.com>
2025-06-24 11:58:13 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Lubiri SS E-Voting Platform',
   description: 'Created by Sseruwagi Sinclaire Sebastian',
+  icon: 'components/favicon.ico', // Path to your icon file in the public directory
   generator: 'Sseruwagi Sinclaire Sebatian',
 }
 
