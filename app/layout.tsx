b31bb46be090eb61a12342e71f6ef2af400e1f20
SSTC-bit <waltdisneyentertainmentco@gmail.com>
2025-06-24 11:56:26 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
      icons: {
         icon: 'components/favicon.ico', // Path to your icon file in the public directory
-        shortcut: 'components/favicon.ico',
+
         apple: 'components/favicon.ico',
         other: {
           rel: 'components/favicon.ico',
